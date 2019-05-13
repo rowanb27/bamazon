317f24f9b214ca9da4cca4904d33bafa69b83a01
Robert Rowan <rowanb27@yahoo.com>
2019-05-13 13:24:37 -0500

    Updated sql

--- bamazon.sql
+++ bamazon.sql
@@ -1,46 +1,30 @@
-DROP DATABASE IF EXISTS bamazon;
+DROP DATABASE IF EXISTS bamazon_DB;
 
-CREATE DATABASE bamazon;
+CREATE DATABASE bamazon_DB;
 
-USE bamazon;
+USE bamazon_DB;
 
-CREATE TABLE products (
-	   item_id INT NOT NULL AUTO_INCREMENT
-	 , product_name VARCHAR(100)
-	 , department_name VARCHAR(100)
-	 , price INT NOT NULL
-	 , stock_quantity INT NOT NULL
-	 , PRIMARY KEY (item_id) 
+CREATE TABLE products(
+    item_ID INT AUTO_INCREMENT PRIMARY KEY,
+    product_Name VARCHAR(255) NOT NULL,
+    department_Name VARCHAR(100) NOT NULL,
+    price DECIMAL (10,2) NOT NULL,
+    stock_Quantity INT NOT NULL
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('iPhone', 'Electronics', 500, 500);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Nintendo Gamecube', 'Electronics', 125, 700);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Baseball Bat', 'Sporting Goods', 25, 500);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Diamond Ring', 'Jewelry', 10000, 10);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Barbie Doll', 'Toys', 15, 1500);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Gnome', 'Lawn and Garden', 25, 3000);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Slinky', 'Toys', 5, 50);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Diamond Bracelot', 'Jewelry', 1200, 125);
-
-INSERT INTO `products` (product_name, department_name, price, stock_quantity)
-VALUES ('Celery', 'Produce', 2, 250);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Tomato', 'Produce', 2, 1200);
-
-SELECT * FROM products;
+INSERT INTO products(product_Name, department_Name, price, stock_Quantity)
+VALUES ("Shampoo", "Hair Care", 22.00, 20),
+       ("Conditioner", "Hair Care", 20.00, 14),
+       ("Tomato", "Produce", 1.99, 56),
+       ("Celery", "Produce", 1.99, 90),
+       ("Paper Towels", "Grocery", 3.95, 30),
+       ("Orange Juice", "Beverages", 3.19, 120),
+       ("Pototo", "Produce", 1.49, 230),
+       ("Band-Aid", "First Aid", 2.29, 40),
+       ("Crest Toothpaste", "Oral Care", 4.99, 52),
+       ("Oral-B Toothbrush", "Oral Care", 5.99, 64),
+       ("Toy Baseball Bat", "Toy", 9.59, 80),
+       ("Baseball Mitt", "Sporting Goods", 7.99, 60),
+       ("Soccer Ball)", "Sporting Goods", 24.99, 48),
+       ("Tennis Ball", "Sporting Goods", 5.99, 36);
+    
